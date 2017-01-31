767e8ecacabad6c78c5e99cf201dfa0955d2ba2d
otyaroo <otyaroo@yahoo.co.jp>
2017-01-31 12:56:26 +0900

    jumpjumpの原型ができた。
    シビアな着地をどうにか…できんかな？

--- New Unity Project 1/Assets/scripts/jumpjump/fallFooting.cs
+++ New Unity Project 1/Assets/scripts/jumpjump/fallFooting.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class fallFooting : MonoBehaviour {
 
+    Rigidbody2D rigid2D;
+    Transform tf;
+
 	// Use this for initialization
 	void Start () {
-		
+        tf = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        tf.Translate(Vector3.down * Time.deltaTime * 0.8f);
 	}
+
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        //Debug.Log("足場, OnCollisionStay2D in");
+        other.transform.Translate(Vector3.down * Time.deltaTime);
+       
+
+    }
 }
+
